6aac3cc162481f0f8010486a85506d3bfb88b61c
kimk <38691828+claudiact6@users.noreply.github.com>
2018-09-22 10:55:45 -0500

    spotify changes

--- assets/app.js
+++ assets/app.js
@@ -5,33 +5,19 @@ var token = "";
 var baseAuth = "https://accounts.spotify.com/authorize?client_id="
 var clientID = "aba6887b24004a76ae602f0e4e0d6da8";
 var responseType = "&response_type=token";
-var redirURI = "&redirect_uri=https://claudiact6.github.io/group-project-1/";
+var redirURI = "&redirect_uri=https://claudiact6.github.io/group-project-1/player/";
 var embedURL = "https://open.spotify.com/embed/";
-//https://accounts.spotify.com/authorize?client_id=aba6887b24004a76ae602f0e4e0d6da8&redirect_uri=https:%2F%2Fclaudiact6.github.io%2Fgroup-project-1%2F&response_type=token
 
 //variables for TicketMaster API
 var countryCode = 'US';
 
 $(document).ready(function () {
-/*     //Get access token from Spotify
-    $.ajax({
-        url: "https://accounts.spotify.com/authorize",
-        method: "GET",
-        client_id: clientID,
-        response_type: "token",
-        redirect_uri:"http://claudiact6.github.io/group-project-1",
-    }).then(function (response) {
-        //Then save token as variable to use later
-        token = response.access_token;
-        console.log(token);
-    }); */
 
     $("#login").on("click", function(event) {
         //Prevent default behavior
         event.preventDefault();
         //redirect to auth
         window.location = baseAuth + clientID + redirURI + responseType;
-        console.log(baseAuth + clientID + redirURI + responseType);
     });
 
     $("#search").on("click", function (event) {
@@ -42,6 +28,9 @@ $(document).ready(function () {
         var splitAuth = authURL.split("&");
         var tokenSplit = splitAuth[0].split("=");
         var token = tokenSplit[1];
+        console.log("splitAuth: " + splitAuth);
+        console.log("tokenSplit: " + tokenSplit);
+        console.log("token: " + token);
         //Assign term to search for based on user input
         searchTerm = $("#userSearch").val();
         console.log(searchTerm);
