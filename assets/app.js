c06629b679e4e100d163431ef20ea772d57d7329
kimk <38691828+claudiact6@users.noreply.github.com>
2018-09-22 10:28:14 -0500

    change type of authentication

--- assets/app.js
+++ assets/app.js
@@ -2,29 +2,43 @@ var urlBase = "https://api.spotify.com/v1/search?q="
 var searchTerm = "";
 var type = "&type=artist%2Calbum%2Ctrack"
 var token = "";
+var baseAuth = "https://accounts.spotify.com/authorize?client_id="
 var clientID = "aba6887b24004a76ae602f0e4e0d6da8";
-var clientSecret = "384ae6bce3aa4ea2910d8065f0a77ea9";
+var responseType = "&response_type=token";
+var redirURI = "&redirect_uri=https://claudiact6.github.io/group-project-1/";
 var embedURL = "https://open.spotify.com/embed/";
+//https://accounts.spotify.com/authorize?client_id=aba6887b24004a76ae602f0e4e0d6da8&redirect_uri=https:%2F%2Fclaudiact6.github.io%2Fgroup-project-1%2F&response_type=token
 
 $(document).ready(function () {
-    //Get access token from Spotify
+/*     //Get access token from Spotify
     $.ajax({
-        url: "https://accounts.spotify.com/api/token",
-        method: "POST",
-        data: {
-            grant_type: "client_credentials"
-        },
-        headers: {
-            Authorization: "Basic " + btoa(clientID + ":" + clientSecret)
-        }
+        url: "https://accounts.spotify.com/authorize",
+        method: "GET",
+        client_id: clientID,
+        response_type: "token",
+        redirect_uri:"http://claudiact6.github.io/group-project-1",
     }).then(function (response) {
         //Then save token as variable to use later
         token = response.access_token;
         console.log(token);
+    }); */
+
+    $("#login").on("click", function(event) {
+        //Prevent default behavior
+        event.preventDefault();
+        //redirect to auth
+        window.location = baseAuth + clientID + redirURI + responseType;
+        console.log(baseAuth + clientID + redirURI + responseType);
     });
+
     $("#search").on("click", function (event) {
         //Prevent default behavior
         event.preventDefault();
+        //Get token from URL
+        var authURL = window.location.href;
+        var splitAuth = authURL.split("&");
+        var tokenSplit = splitAuth[0].split("=");
+        var token = tokenSplit[1];
         //Assign term to search for based on user input
         searchTerm = $("#userSearch").val();
         console.log(searchTerm);
